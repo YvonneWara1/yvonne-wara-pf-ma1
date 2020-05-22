5f61575a72d6e4b3db684a1be92a9d3b27c265e1
YvonneWara1 <61632053+YvonneWara1@users.noreply.github.com>
2020-05-22 19:45:20 +0200

    Add files via upload

---
+++ js/js/script.js
@@ -0,0 +1,86 @@
+// question 1
+
+var firstVar;
+
+//question 2
+
+var name = "Yvonne";
+
+// question 3
+
+var number = 19; 
+
+// question 4 
+
+var division = (20 / 5);
+
+// question 5
+
+var animal = "frog";
+console.log(animal);
+
+console.log(typeof "frog");
+
+// question 6
+
+var orderHasShipped = true;
+
+// question 7
+
+if (orderHasShipped = true); {
+    console.log("true");
+}
+
+// question 8 
+
+for (var count =0; count <10; count++) {
+    console.log(count);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
